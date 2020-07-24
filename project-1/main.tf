ff501d10aece3e8be9be26ac687cbf6daebbb335
Carl Cliver <carl@Carls-MacBook-Air-2.local>
2020-07-23 21:54:38 -0400

    Adding cloudfront

--- project-1/main.tf
+++ project-1/main.tf
@@ -34,3 +34,42 @@ resource "aws_s3_bucket" "website" {
 }
 POLICY
 }
+
+resource "aws_cloudfront_distribution" "website" {
+  origin {
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id = "S3-udacity-website"
+  }
+
+  enabled = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-udacity-website"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
--- project-1/outputs.tf
+++ project-1/outputs.tf
@@ -1,3 +1,7 @@
 output "bucket_name" {
   value = aws_s3_bucket.website.id
 }
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.website.domain_name
+}
